61db0ef3c6af84a694a81dbb5a157dfd0d9d93cd
Jasper Young <jasper.c.young@gmail.com>
2024-06-04 22:32:46 -0400

    Expand C++ timer interface

--- cpp/timer/timer.hpp
+++ cpp/timer/timer.hpp
@@ -2,20 +2,24 @@
 #define BITS_AND_BYTES_TIMER_HPP
 
 #include <chrono>
+#include <ratio>
 #include <utility>
+
+#include "../type_traits/type_traits.hpp"
 
 namespace bits_and_bytes {
 
+template <typename t_duration = std::chrono::duration<double, std::ratio<1>>,
+          typename t_clock = std::chrono::steady_clock>
+  requires(bits_and_bytes::type_traits::is_specialization_of_v<
+               t_duration, std::chrono::duration>,
+           std::chrono::is_clock_v<t_clock>)
 class timer {
 private:
-  using clock_type = std::chrono::steady_clock;
-  using second_type = std::chrono::duration<double, std::ratio<1>>;
-
-private:
-  std::chrono::time_point<clock_type> m_time;
+  std::chrono::time_point<t_clock> m_time;
 
 public:
-  timer() : m_time{clock_type::now()} {}
+  timer() : m_time{t_clock::now()} {}
 
   timer(const timer &other) : m_time{other.m_time} {}
 
@@ -24,12 +28,13 @@ public:
   ~timer() {}
 
 public:
-  void reset() { m_time = clock_type::now(); }
+  void reset() { m_time = t_clock::now(); }
 
-  double elapsed() const {
-    return std::chrono::duration_cast<second_type>(clock_type::now() - m_time)
-        .count();
+  t_duration elapsed() const {
+    return std::chrono::duration_cast<t_duration>(t_clock::now() - m_time);
   }
+
+  double elapsed_c() const { return elapsed().count(); }
 
 public:
   timer &operator=(const timer &other) {
