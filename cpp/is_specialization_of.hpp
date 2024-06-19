a896d30bc8f20e1a547ee086cd3b7bd31e1eb113
Jasper Young <jasper.c.young@gmail.com>
2024-06-19 16:33:17 -0400

    Re-organize C++ stuff

--- cpp/is_specialization_of.hpp
+++ cpp/is_specialization_of.hpp
@@ -1,11 +1,10 @@
-#ifndef BITS_AND_BYTES_TYPE_TRAITS_HPP
-#define BITS_AND_BYTES_TYPE_TRAITS_HPP
+#ifndef BITS_AND_BYTES_IS_SPECIALIZATION_OF_HPP
+#define BITS_AND_BYTES_IS_SPECIALIZATION_OF_HPP
 
 #include <type_traits>
-template <typename T> class C {};
 
 namespace bits_and_bytes {
-namespace type_traits {
+namespace is_specialization_of {
 
 template <typename t_type, template <typename...> typename t_template>
 struct is_specialization_of : std::false_type {};
@@ -18,7 +17,7 @@ template <typename t_type, template <typename...> typename t_template>
 inline constexpr bool is_specialization_of_v =
     is_specialization_of<t_type, t_template>::value;
 
-} // namespace type_traits
+} // namespace is_specialization_of
 } // namespace bits_and_bytes
 
 #endif
